78d854199b515b44122a4cb65c5a26ffe4a5b0e5
Kang-SeoHyun <kang061020@gmail.com>
2022-11-20 15:21:44 +0900

    add: 11/20

---
+++ week1/13699.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+t = [0] * 36
+t[0] = 1
+t[1] = 1
+
+if n > 1:
+	for i in range(2, n + 1):
+		for j in range(i):
+			t[i] += t[j] * t[i - j - 1]
+
+print(t[n])
--- week1/17202.py
+++ week1/17202.py
@@ -1,1 +1,19 @@
-num_list = list(map(int, input().split()))
+a = list(input())
+b = list(input())
+answer = ''
+
+def add(i, j):
+	i = int(i)
+	j = int(j)
+	return str((i + j) % 10)
+
+for i in range(8):
+	answer += a[i] + b[i]
+
+while len(answer) != 2:
+	temp = []
+	for i in range(len(answer) - 1):
+		temp.append(add(answer[i], answer[i+1]))
+	answer = ''.join(temp)
+
+print(answer)
