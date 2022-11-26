e35d05aaff69c14e14bdebcb658505060859d0df
Kang-SeoHyun <kang061020@gmail.com>
2022-11-26 18:42:16 +0900

    add: saturday start

--- week2/1032.py
+++ week2/1032.py
@@ -1,6 +1,10 @@
 num = int(input())
 target1 = list(str(input()))
 
-for i in range (0, num):
+for i in range (0, num - 1):
 	target2 = list(str(input()))
-	for j in 
+	for j in range (len(target2)):
+		if target1[j] != target2[j]:
+			target1[j] = '?'
+
+print(''.join(target1))
