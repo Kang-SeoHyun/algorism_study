c9e8ae7d419c85b268d386867b49b05b6041f57c
Kang-SeoHyun <kang061020@gmail.com>
2022-12-06 21:43:09 +0900

    fail 3079

---
+++ week3/3079.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+time = [int(input()) for _ in range(n)]
+
+start = 0
+end = max(time) * M
+result = 0
+
+while start <= end :
+	mid = (start + end) // 2
+	
+	if 
