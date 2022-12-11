8701c671d11918fabf2f115f6c94250ac19b6774
Kang-SeoHyun <kang061020@gmail.com>
2022-12-11 15:37:09 +0900

    add: 3079 fix

--- week3/3079.py
+++ week3/3079.py
@@ -2,13 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-time = [int(input()) for _ in range(n)]
+time = [int(input()) for _ in range(N)]
 
-start = 0
+start = min(time)
 end = max(time) * M
-result = 0
+result = max(time) * M
 
 while start <= end :
 	mid = (start + end) // 2
+	total = 0 #입국 할 수 있는 사람 수
+	for i in range(N):
+		total += mid // time[i]
 	
-	if 
