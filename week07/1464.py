f1cd9d8def533bb2301772859da78833163a8441
Kang-SeoHyun <kang061020@gmail.com>
2023-01-01 18:38:20 +0900

    add: 1464

--- week07/1464.py
+++ week07/1464.py
@@ -2,15 +2,24 @@
 import sys
 input = sys.stdin.readline
 
-s = list(str(input()))
+s = list(str(input().rstrip()))
+n = len(s)
 
-def reverse(num, iterable):
+def reverse(num, str):
 	left = 0
 	right = num - 1
 	while left < right:
-		temp = iterable[right]
-		iterable[right] = iterable[left]
-		iterable[left] = temp
+		temp = str[right]
+		str[right] = str[left]
+		str[left] = temp
 		left += 1
 		right -= 1
-	return iterable
+	return str
+
+for i in range(1, n):
+	if s[i] < s[i - 1] and s[i] <= s[0]:
+		reverse(i, s)
+		if s[i - 1] >= s[i]:
+			reverse(i + 1, s)
+
+print(''.join(s))
